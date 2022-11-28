50c639e230f5acfddb4c2c627c977ff8321fef46
Ravichandra V <pradi.ravi@gmail.com>
2022-11-28 16:39:27 +0530

    Adding CustomScript AB#7

---
+++ js/customScript.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.col-xs-1').click(function(){
+    if ($(this).text().trim() !== '='){
+      var text = $(this).text().toString()
+      $('input:text').val(function(i, val){
+        return val + text.trim();
+      });
+    }
+  })
+  $('.btn-warning').click(function(){
+    $('input:text').val('');
+  })
+  $('#submit').click(function(){
+    var expr = $('input').val();
+    var res = eval(expr);
+    $('input:text').val(res);
+  })
+});
